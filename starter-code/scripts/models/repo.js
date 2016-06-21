9e8fc16ad6fe95e4f247fee901da13a75814b122
John Gaines <gaines.johnp@gmail.com>
2016-06-21 13:31:39 -0700

    finished the first half of the TODOs for the lab.

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,14 @@
   repos.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/steventhan/repos',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        console.log(data);
+      }
+    });
   };
 
   repos.withTheAttribute = function(myAttr) {
