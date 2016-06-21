b61b88c76e58918ca464e6f61a9488bfee0cd440
Code Fellows <codefellows@Stevens-MacBook-Pro.local>
2016-06-21 14:34:12 -0700

    finishing last TODO

--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -1,9 +1,13 @@
 (function(module) {
   var repoView = {};
-  /* TODO: Let's compile our new template!
+  /* DONE: Let's compile our new template!
        Save the result of invoking Handlebars in this 'repoCompiler' variable
        that we will pass to the append method below. */
-  var repoCompiler;  // Finish the Handlebars method here!
+  var repoCompiler = function() {
+    var template = Handlebars.compile($('#repo-template').html());
+    return template(this);
+    console.log('repo compiler');
+  };  // Finish the Handlebars method here!
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
@@ -11,8 +15,10 @@
       .map(repoCompiler)
     );
   };
-/* TODO: Call the function that loads (or 'requests') our repo data.
+/* DONE: Call the function that loads (or 'requests') our repo data.
     Pass in some view funcyion as a higher order callback, so our repos
     will render after the data is loaded. */
 
-});
+  repos.requestRepos(repoView.renderRepos);
+  module.repoView = repoView;
+})(window);
